2b4e40d1275da064bd9e3bda4a19259ff59eabbc
Neil Kulkarni <neil_kulkarni@icloud.com>
2017-01-11 21:00:29 -0500

    This marks the initial commit.

--- Final Grade Calculator/ViewController.swift
+++ Final Grade Calculator/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var currentGradeField: UITextField!
+    @IBOutlet weak var desiredGradeField: UITextField!
+    @IBOutlet weak var weightFinalField: UITextField!
+    @IBOutlet weak var gradeNeededLabel: UILabel!
+    @IBOutlet weak var errorLabel: UILabel!
+    @IBOutlet weak var calculateGradeNeededButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +29,40 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func onCalculateGrade(_ sender: Any) {
+        print("Calculating!")
+        
+        guard let currentGrade = currentGradeField.text, !currentGrade.isEmpty else {
+            errorLabel.isHidden = false
+            errorLabel.text = "Please enter your current grade."
+            return
+        }
+        
+        guard let desiredGrade = desiredGradeField.text, !desiredGrade.isEmpty else {
+            errorLabel.isHidden = false
+            errorLabel.text = "Please enter your desired grade."
+            return
+        }
+        
+        guard let weightFinal = weightFinalField.text, !weightFinal.isEmpty else {
+            errorLabel.isHidden = false
+            errorLabel.text = "Please enter the weight of your final."
+            return
+        }
+        
+        let currentGradeValue = Double(currentGrade)!
+        let desiredGradeValue = Double(desiredGrade)!
+        let weightFinalValue = Double(weightFinal)!
+        
+        // unweightedGrade = ((100 - weightFinal) / 100) * currentGrade
+        // differenceGrade = gradeWanted - gradeHave
+        // diffGrade / weightFinal * 100
+        
+        let unweightedGrade = ((100 - weightFinalValue) / 100) * currentGradeValue
+        let differenceGrade = desiredGradeValue - unweightedGrade
+        let gradeNeeded = differenceGrade / weightFinalValue * 100
+    
+        gradeNeededLabel.text = "\(gradeNeeded)%"
+    }
 }
 
